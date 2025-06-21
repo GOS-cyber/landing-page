51cd55073b3ae150f07278f857211baad711f464
Zecalouco <queroserlouco@gmail.com>
2025-06-20 23:40:49 -0300

    fix: adiciona base para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/landing-page/',
+  base:  '/landing-page/', // ajuste de base
   plugins: [tailwindcss()],
 });
